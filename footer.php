b0810de81a546154d072c52530f087e8416a5712
samiksha1309 <samiksha.13agrawal@gmail.com>
2025-06-19 22:35:52 +0530

    Initial commit

---
+++ footer.php
@@ -0,0 +1,252 @@
+    <!-- WD Get-app -->
+  <section class="sc-getapp">
+    <div class="tf-container">
+      <div class="row align-item-center">
+        <div class="col-lg-6 wow fadeInLeft">
+          <div class="wd-get-app style-1">
+            <div class="thumb ani4">
+              <img src="images/review/phone.png" alt="images">
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 wow fadeInRight">
+          <div class="wd-get-app">
+            <div class="content">
+              <h4>Download The App</h4>
+              <p>Structured digital interviews increase the predictive <br> validity of your hires by 65%.</p>
+              <div class="group-btn">
+                <a href="#"><img src="images/review/btn2.png" alt="images"></a>
+                <a href="#"><img src="images/review/btn1.png" alt="images"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+  <!-- End WD-Get-app -->
+  <!-- wd-partner -->
+  <section>
+    <div class="wd-partner">
+      <div class="tf-container">
+        <h1 class="title-partner">
+          Over 100,000 recruiters use Jobtex to modernize their hiring
+        </h1>
+        <div class="swiper partner-type-6">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <a class="logo-partner" href="#">
+                <img src="images/partners/Logo.png" alt="images/partners/Logo.png">
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a href="#" class="logo-partner">
+                <img src="images/partners/Logo-1.png" alt="images/partners/Logo.png">
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a href="#" class="logo-partner">
+                <img src="images/partners/Logo-2.png" alt="images/partners/Logo.png">
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a href="#" class="logo-partner">
+                <img src="images/partners/Logo-3.png" alt="images/partners/Logo.png">
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a href="#" class="logo-partner">
+                <img src="images/partners/Logo-4.png" alt="images/partners/Logo.png">
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a href="#" class="logo-partner">
+                <img src="images/partners/Logo-5.png" alt="images/partners/Logo.png">
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a href="#" class="logo-partner">
+                <img src="images/partners/Logo.png" alt="images/partners/Logo.png">
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a href="#" class="logo-partner">
+                <img src="images/partners/Logo-1.png" alt="images/partners/Logo.png">
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a href="#" class="logo-partner">
+                <img src="images/partners/Logo-2.png" alt="images/partners/Logo.png">
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a href="#" class="logo-partner">
+                <img src="images/partners/Logo-3.png" alt="images/partners/Logo.png">
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a href="#" class="logo-partner">
+                <img src="images/partners/Logo-4.png" alt="images/partners/Logo.png">
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a href="#" class="logo-partner">
+                <img src="images/partners/Logo-5.png" alt="images/partners/Logo.png">
+              </a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--End wd-partner -->
+  <footer class="footer">
+    <div class="top-footer">
+      <div class="tf-container">
+        <div class="row">
+          <div class="col-lg-2 col-md-4">
+            <div class="footer-logo">
+              <img src="images/logo.png" alt="images">
+            </div>
+          </div>
+          <div class="col-lg-10 col-md-8">
+            <div class="wd-social d-flex aln-center">
+              <span>Follow Us:</span>
+              <ul class="list-social d-flex aln-center">
+                <li><a href="#"><i class="icon-facebook"></i></a></li>
+                <li><a href="#"><i class="icon-linkedin2"></i></a></li>
+                <li><a href="#"><i class="icon-twitter"></i></a></li>
+                <li><a href="#"><i class="icon-pinterest"></i></a></li>
+                <li><a href="#"><i class="icon-instagram1"></i></a></li>
+                <li><a href="#"><i class="icon-youtube"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="inner-footer">
+      <div class="tf-container">
+        <div class="row">
+          <div class="col-lg-4 col-md-6">
+            <div class="footer-cl-1">
+              <div class="icon-infor d-flex aln-center">
+                <div class="icon">
+                  <span class="icon-call-calling"><span class="path1"></span><span class="path2"></span><span
+                      class="path3"></span><span class="path4"></span></span>
+                </div>
+                <div class="content">
+                  <p>Need help? 24/7</p>
+                  <h6><a href="tel:0123456678">001-1234-88888</a></h6>
+                </div>
+              </div>
+              <p>Job Searching Just Got Easy. Use Jobtex to run a hiring site and earn money in the process!</p>
+              <div class="ft-icon"> <i class="icon-map-pin"></i> 101 E 129th St, East Chicago, IN 46312, US</div>
+              <form action="#" id="subscribe-form">
+                <input type="email" placeholder="Your email address" required="" id="subscribe-email">
+                <button class="tf-button" type="submit" id="subscribe-button"><i
+                    class="icon-paper-plane-o"></i></button>
+              </form>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-6 col-6">
+            <div class="footer-cl-2">
+              <h6 class="ft-title">
+                Quick Links
+              </h6>
+              <ul class="navigation-menu-footer">
+                <li> <a href="find-jobs-list.html">Job Packages</a> </li>
+                <li> <a href="find-jobs-list.html">Post New Job</a> </li>
+                <li> <a href="find-jobs-list.html">Jobs Listing</a> </li>
+                <li> <a href="find-jobs-list.html">Jobs Style Grid</a> </li>
+                <li> <a href="employers-list.html">Employer Listing</a> </li>
+                <li> <a href="employers-grid-fullwidth.html">Employers Grid</a> </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-4 col-6">
+            <div class="footer-cl-3">
+              <h6 class="ft-title">
+                For Candidates
+              </h6>
+              <ul class="navigation-menu-footer">
+                <li> <a href="dashboard.html">User Dashboard</a> </li>
+                <li> <a href="sample-cv.html">CV Packages</a> </li>
+                <li> <a href="candidate-list-sidebar.html">Candidate Listing</a> </li>
+                <li> <a href="candidate-grid.html">Candidates Grid</a> </li>
+                <li> <a href="about-us.html">About us</a> </li>
+                <li> <a href="contact-us.html">Contact us </a> </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-4 col-6">
+            <div class="footer-cl-4">
+              <h6 class="ft-title">
+                For Employers
+              </h6>
+              <ul class="navigation-menu-footer">
+                <li> <a href="find-jobs-list.html">Post New Job</a> </li>
+                <li> <a href="employers-list.html">Employer Listing</a> </li>
+                <li> <a href="employers-grid-sidebar.html">Employers Grid</a> </li>
+                <li> <a href="find-jobs-list.html">Job Packages</a> </li>
+                <li> <a href="find-jobs-list.html">Jobs Listing</a> </li>
+                <li> <a href="find-jobs-grid.html">Jobs Style Grid</a> </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-4 col-6">
+            <div class="footer-cl-5">
+              <h6 class="ft-title">
+                Download App
+              </h6>
+              <ul class="ft-download">
+                <li> <a href="#"><img src="images/review/btn3.png" alt="images"></a></li>
+                <li> <a href="#"><img src="images/review/btn4.png" alt="images"></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bottom">
+      <div class="tf-container">
+        <div class="row">
+          <div class="col-lg-6 col-md-6">
+            <div class="bt-left">
+              <div class="copyright">©2023 Themesflat. All Rights Reserved.</div>
+              <span class="select-language">
+                <select class="image-select">
+                    <option data-thumbnail="images/review/flag.png">English</option>
+                    <option data-thumbnail="images/review/flag3.png">USA</option>
+                    <option data-thumbnail="images/review/flag2.png">VN</option>
+                </select>
+            </span>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-6">
+            <ul class="menu-bottom d-flex aln-center">
+              <li><a href="term-of-use.html">Terms Of Services</a> </li>
+              <li><a href="pricing.html">Privacy Policy</a> </li>
+              <li><a href="contact-us.html">Cookie Policy</a> </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  </div><!-- /.boxed --> 
+
+  <script src="javascript/jquery.min.js"></script>
+  <script src="javascript/swiper-bundle.min.js"></script>
+  <script src="javascript/bootstrap.min.js"></script>
+  <script src="javascript/boostrap-select.min.js"></script>
+  <script src='javascript/jquery.nice-select.min.js'></script>
+  <script src="javascript/countto.js"></script>
+  <script src="javascript/wow.min.js"></script>
+  <script src="javascript/swiper.js"></script>
+  <script src="javascript/plugin.min.js"></script>
+  <script src="javascript/jquery.cookie.js"></script>
+  <script src="javascript/switcher.js"></script>
+  <script src="javascript/main.js"></script>
